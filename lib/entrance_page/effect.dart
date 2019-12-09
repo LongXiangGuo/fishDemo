f674d6d99a9e1c91e1e4d50a253c35df5ab113de
longxiang <longxiang@guolongxaingMacBook.local>
2019-12-10 00:31:20 +0800

    fix demo

--- lib/entrance_page/effect.dart
+++ lib/entrance_page/effect.dart
@@ -1,17 +1,21 @@
+
 import 'package:fish_redux/fish_redux.dart';
-import 'package:flutter/widgets.dart' hide Action;
+import 'package:flutter/widgets.dart' hide Action; //注意1
 import 'action.dart';
 import 'state.dart';
 
 Effect<EntranceState> buildEffect() {
   return combineEffects(<Object, Effect<EntranceState>>{
     EntranceAction.action: _onAction,
-    EntranceAction.action: _onOpenGrid,
+    // EntranceAction.action: _onOpenGrid,
+     EntranceAction.openGrid: _onOpenGrid,
   });
 }
 
 void _onAction(Action action, Context<EntranceState> ctx) {}
 
+///跳转方法实现
 void _onOpenGrid(Action action, Context<EntranceState> ctx) {
-  Navigator.of(ctx.context).pushNamed('grid_page', arguments: null);
+  Navigator.of(ctx.context).pushNamed("grid_page", arguments: null);
 }
+
--- lib/grid_page/view.dart
+++ lib/grid_page/view.dart
@@ -1,7 +1,5 @@
 import 'package:fish_redux/fish_redux.dart';
-import 'package:flutter/material.dart';
-
-import 'action.dart';
+import 'package:flutter/material.dart' hide Action;
 import 'state.dart';
 
 Widget buildView(GridState state, Dispatch dispatch, ViewService viewService) {
@@ -9,30 +7,31 @@ Widget buildView(GridState state, Dispatch dispatch, ViewService viewService) {
     appBar: AppBar(
       title: Text('grid page'),
     ),
-    body: GridView.count(
-      crossAxisCount: 2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
-      childAspectRatio: 1,
-      padding: EdgeInsets.all(20),
-      children: List.generate(state.models.length, (index) {
-        return Center(
-          child: Card(
-            color: Colors.lightBlueAccent,
-            child: InkWell(
-              splashColor: Colors.blue.withAlpha(100),
-              onTap: () {},
-              child: Container(
-                width: 200,
-                height: 200,
-                child: Center(
-                  child: Text(state.models[index].name),
-                ),
-              ),
-            ),
-          ),
-        );
-      }),
-    ),
+    body: Text('test'),
+    // body: GridView.count(
+    //   crossAxisCount: 2,
+    //   crossAxisSpacing: 20,
+    //   mainAxisSpacing: 20,
+    //   childAspectRatio: 1,
+    //   padding: EdgeInsets.all(20),
+    //   children: List.generate(state.models.length, (index) {
+    //     return Center(
+    //       child: Card(
+    //         color: Colors.lightBlueAccent,
+    //         child: InkWell(
+    //           splashColor: Colors.blue.withAlpha(100),
+    //           onTap: () {},
+    //           child: Container(
+    //             width: 200,
+    //             height: 200,
+    //             child: Center(
+    //               child: Text(state.models[index].name),
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     );
+    //   }),
+    // ),
   );
 }
